c319fb4fe42a0bd8ab4b89c763844c5274086004
Grove1212 <stella223@korea.ac.kr>
2023-09-02 21:22:32 +0900

    230902

---
+++ Ex07_1/07_8.c
@@ -0,0 +1,18 @@
+/*
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char str1[20], str2[20];
+
+	strcpy(str1, "hello");
+	printf("str1 = %s\n", str1);
+
+	strcpy(str2, str1);
+	printf("str2 = %s\n", str2);
+
+	return 0;
+}*/
