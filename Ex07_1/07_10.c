3e652dc452c61d85890e8089381484ca037b4f76
Grove1212 <stella223@korea.ac.kr>
2023-09-03 09:52:06 +0900

    230903

---
+++ Ex07_1/07_10.c
@@ -0,0 +1,24 @@
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char str1[20];
+	char str2[20] = "ABCDE";
+
+	strcpy_s(str1,20, "abcde");
+
+	if(strcmp(str1, "abcde") == 0)
+		printf("두 문자열은 같습니다.\n");
+	else
+		printf("두 문자열은 다릅니다.\n");
+
+
+	if (strcmp(str1, str2) == 0)
+		printf("두 문자열은 같습니다.\n");
+	else
+		printf("두 문자열은 다릅니다.\n");
+
+	return 0;
+}*/
---
+++ Ex07_1/07_9.c
@@ -0,0 +1,15 @@
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char str1[] = "abcde";
+	char str2[10] = "";
+
+	printf("str1의 길이 : %zd\n", strlen(str1));
+	printf("str2의 길이 : %zd\n", strlen(str2));
+	printf("문자열의 길이 : %zd\n", strlen("hello world"));
+
+	return 0;
+}*/
