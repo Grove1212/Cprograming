e3e79b7767224c5e5cb90d026a9c108ff637e831
Grove1212 <stella223@korea.ac.kr>
2023-09-14 11:35:12 +0900

    230914 11:35

---
+++ Ex07_1/CountHanGul.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	float ki = 0.0;
+	char a[11] = "안녕하세요";  // [3]에서 [11]로 늘어났어요! 글자에 따라 어떻게 늘어나시는지 감이 오시나요??
+	// 네!! 글자수 X 2 + 1입니다 ㅎㅎ
+
+	printf("%s", a);
+
+	return 1;
+}
